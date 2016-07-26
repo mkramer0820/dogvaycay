1f9575ece746b72229752bf5494fe0a61646234b
mkramer0820 <mkramer0820@gmail.com>
2016-07-25 22:37:54 -0400

    added user creation form

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     # login page
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout/$', views.logout_view, name='logout')
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register'),
+    #url(r'^register/$', views.register, name='registration_form'),
 ]
+
